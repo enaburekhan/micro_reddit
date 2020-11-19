82855263d555776b8b5c80be39da2c77c216b9af
Gzim <gzimasanni@gmail.com>
2020-11-19 11:28:01 +0100

    Fix linter errors for frozen strings

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 
 class Comment < ApplicationRecord
   validates :comment, presence: true, length: { minimum: 10, maximum: 80 }
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 
 class Post < ApplicationRecord
   validates :title, presence: true, length: { minimum: 5, maximum: 20 }
